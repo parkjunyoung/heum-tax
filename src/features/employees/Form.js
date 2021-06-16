cd574b991d48df6c4c4e9c3d05963503323e8f40
pjoon86 <pjoon86@naver.com>
2021-06-16 21:44:02 +0900

    element ui react setting

--- src/features/employees/Form.js
+++ src/features/employees/Form.js
@@ -1,3 +1,15 @@
+import { Input } from 'element-react';
+
 export default function Form(){
-  return <>개인정보</>
+  return <>
+    <form action="">
+      개인정보
+      성명
+      이메일
+      내외국인 구분
+      주민등록번호
+      현재 대한민국에 거주(체류)하고 있습니까?
+      <Input type="text" />
+    </form>
+  </>
 }

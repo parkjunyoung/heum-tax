705f2eea284165848aa83caa9dbb404565ef250e
pjoon86 <pjoon86@naver.com>
2021-06-16 20:45:22 +0900

    설정 index.js 이동

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import Employees from './features/employees/Employees'
 
-import { Provider } from 'react-redux';
-import createStore from './store/store';
-export const store = createStore();
-
 function App() {
   return (
-    <Provider store={store}>
       <Employees />
-    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import createStore from './store/store';
+
+export const store = createStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider> ,
   document.getElementById('root')
 );
 
